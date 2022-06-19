79754ef4651bb9e59b7e417cf7b56717d8dd109f
fortunatozm <fortunatompongo@hotmail.com>
2022-06-18 21:08:36 -0300

    8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'. - Corrigindo2

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 CREATE TABLE criada (
-	column1 int,
-	column2 varchar(50),
-	column3 int
+	A int,
+	Trybe varchar(50),
+	eh int
 );
 
-insert criada (column1, column2, column3) values (5+6, 'de', 2+8 );
+insert criada (A, Trybe, eh) values (5+6, 'de', 2+8 );
 
-select column1 as 'A', column2 as 'Trybe', column3 as 'eh' from northwind.criada
+select A, Trybe, eh from northwind.criada
