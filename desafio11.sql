72cf54b998f239fdc31e86119f39e67f41db8fcb
fortunatozm <fortunatompongo@hotmail.com>
2022-06-18 21:53:58 -0300

    11 - Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39. - OK

---
+++ desafio11.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM northwind.purchase_orders
+WHERE notes = 'Purchase generated based on Order #30' or notes = 'Purchase generated based on Order #31' or notes = 'Purchase generated based on Order #32' or notes = 'Purchase generated based on Order #33' or notes = 'Purchase generated based on Order #34' or notes = 'Purchase generated based on Order #35' or notes = 'Purchase generated based on Order #36' or notes = 'Purchase generated based on Order #37' or notes = 'Purchase generated based on Order #38' or notes = 'Purchase generated based on Order #39'
